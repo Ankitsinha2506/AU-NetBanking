89498ef57b2b173608a9610c493d8c219ad44eea
Pranjal Supekar <pranjalsupekar91@gmail.com>
2026-06-09 12:03:00 +0530

    not change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import HeroSlider from "./components/HeroSlider";
 import ExploreProducts from "./components/ExploreProducts";
 import BlogMedia from "./components/BlogMedia";
 import StayInformed from "./components/StayInformed";
-import Footer from "./components/Footer";
 
 export default function App() {
   return (
